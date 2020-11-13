f41e44d9f02a8c899e5c7c77daeefced3adb307b
pbalint99 <41841698+pbalint99@users.noreply.github.com>
2020-11-13 17:29:11 +0100

    Ikoncsere

--- app/src/main/java/hu/bme/aut/android/studyplanner/TaskListActivity.kt
+++ app/src/main/java/hu/bme/aut/android/studyplanner/TaskListActivity.kt
@@ -12,6 +12,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 
